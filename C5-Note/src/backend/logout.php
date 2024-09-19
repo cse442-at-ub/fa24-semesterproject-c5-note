b7f6bcad6ba7cf985d792073205cee61a0ea9b84
mdrum617 <mdrum617@gmail.com>
2024-09-19 16:16:49 -0400

    Implemented logout API

--- C5-Note/src/backend/logout.php
+++ C5-Note/src/backend/logout.php
@@ -1,1 +1,15 @@
 <?php
+
+if(isset($_COOKIE["username"])) {
+
+    $connection = new mysqli("localhost:3306", $username, $password, $db_name);
+
+    $result = $connection->query("SELECT * FROM users WHERE username = $_COOKIE['username']");
+    $logout_id = $result->fetch_assoc()["user_id"];
+
+    $connection = new mysqli("localhost:3306", $username, $password, $db_name);
+    $result = $connection->query("DELETE FROM active_users WHERE user_id = $logout_id");
+}
+
+setcookie("username", "", time() - 60);
+setcookie("token", "", time() - 60);
