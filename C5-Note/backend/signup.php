6d5dea401bd521a72bc943f2823bfd4331a651c1
mrum617 <mdrum2@buffalo.edu>
2024-09-19 13:30:11 -0400

    Added mysqli to insert into database
    This will not work as is, since the variables used in line #14 are not set. I still need to figure out how to include these without having them in the source code itself.

--- C5-Note/backend/signup.php
+++ C5-Note/backend/signup.php
@@ -1,4 +1,31 @@
 <?php
 
+/*
+
+We need to set $username, $password, $db_name, but I don't know what those are yet.
+
+*/
+
 $json = json_decode(file_get_contents("php://input"));
 
+$signup_name = $json->username;
+$signup_pass = password_hash($json->password, PASSWORD_DEFAULT);
+
+$connection = new mysqli("localhost:3306", $username, $password, $db_name);
+
+
+if($connection->connect_error) {
+       	die("Could not connect to the database");
+}
+
+
+try {
+    
+    $result = $connection->query("INSERT INTO users (username, password) VALUES ('$signup_name', '$signup_pass')");
+
+    http_response_code(201);
+}
+catch(Exception $e) {
+
+	http_response_code(409);
+}
