3bd7dfe32fbf5e6d41547ba10ae8cd9914488be9
mdrum617 <mdrum617@gmail.com>
2024-09-19 18:28:08 -0400

    Logs out username from request, not cookies

--- C5-Note/src/backend/logout.php
+++ C5-Note/src/backend/logout.php
@@ -3,20 +3,22 @@
 $config = file_get_contents("../config.json");
 $data = json_decode($config);
 
+$json = json_decode(file_get_contents("php://input"));
+
+$logout_name = $json->username;
+
 $username = $data->username;
 $password = $data->password;
 $db_name = $data->db_name;
 
-if(isset($_COOKIE["username"])) {
+$connection = new mysqli("localhost:3306", $username, $password, $db_name);
+$result = $connection->query("SELECT * FROM users WHERE username = '$logout_name'");
 
-    $connection = new mysqli("localhost:3306", $username, $password, $db_name);
-
-    $result = $connection->query("SELECT * FROM users WHERE username = $_COOKIE['username']");
+if($result->num_rows > 0) {
     $logout_id = $result->fetch_assoc()["user_id"];
 
     $connection = new mysqli("localhost:3306", $username, $password, $db_name);
     $result = $connection->query("DELETE FROM active_users WHERE user_id = $logout_id");
 }
-
 setcookie("username", "", time() - 60);
 setcookie("token", "", time() - 60);
