83acde22af52e80574dd7fbd43730f2580a15fa1
Chris Ansah <ckansah@buffalo.edu>
2024-09-20 12:56:11 -0400

    further styling tweaks

--- C5-Note/src/frontend/profile.jsx
+++ C5-Note/src/frontend/profile.jsx
@@ -20,13 +20,20 @@ export function Profile(){
         <Top_bar/>
         <form action='/route' method='post'>
           <div id="Profile_Info" className='container_text'>
-        
-
           
+          <article className="profile_info">
+          <div class="circle">
+          <p class="text"></p>
+          </div>
+          <Link to="/"><button className="take_notes_button">Take Notes</button></Link>
+              <h1>Jane Doe</h1>
+          
+          </article>
+
           </div>
           <div className='Profile_Buttons'>
-          <Link to="/notes"><button className="take_notes_button">Take Notes</button></Link> 
-          <Link to="/home"><button className="log_out_button">Log Out</button></Link> 
+          <Link to="/"><button className="take_notes_button">Take Notes</button></Link> 
+          <Link to="/"><button className="log_out_button">Log Out</button></Link> 
           </div>    
         </form>
         </>
