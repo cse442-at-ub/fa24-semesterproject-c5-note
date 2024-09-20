b429a31377f255862ffc53eed693954d6c163763
Collin <99486132+CollinOlkows@users.noreply.github.com>
2024-09-20 04:54:46 -0400

    Converted the application to use Hash Router as BrowserRouter Would not retain memory of page. Also implemented basic backend connection

--- C5-Note/src/App.jsx
+++ C5-Note/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
+import { HashRouter  as Router,Routes,Route,Link } from "react-router-dom";
 import { Home } from './Pages/home';
 import { Page1 } from './Pages/page1';
 import { Simple_notebook } from './Pages/simple_notebook';
 import { PageNotebooks } from './Pages/notebooks';
+
+
 
 function App() {
 
@@ -12,10 +14,14 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home/> }/>
         <Route path="/next" element={ <Page1/> }/>
+        <Route path="/note" element={ <Simple_notebook/> }/>
+        <Route path="/notebooks" element = { <PageNotebooks/> }/>
       </Routes>
     </Router>
   )
 
 }
+
+
 
 export default App;
--- C5-Note/src/Pages/home.jsx
+++ C5-Note/src/Pages/home.jsx
@@ -1,7 +1,7 @@
 import logo from '../C5.png';
 import '../App.css';
 import './home.css';
-import { Link } from "react-router-dom";
+import { Link,useNavigate  } from "react-router-dom";
 
 export function Top_bar(){
   return(
@@ -13,20 +13,52 @@ export function Top_bar(){
   )
 }
 
+
+
 export function Home(){
+    const navigate = useNavigate();
+
+    const login = ()=>{
+      if(document.getElementById("username").value == ''){
+        console.log('here')
+        alert('Please enter a username')
+      }else if(document.getElementById("password").value == ''){
+        console.log('here')
+        alert('Please enter a password')
+      }else{
+      fetch("backend/login.php", {
+        method: "POST",
+        body: JSON.stringify({
+          username: document.getElementById("username").value,
+          password: document.getElementById("password").value,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          if(json.status =='failed'){
+            alert('Username or Password is incorrect')
+          }else{
+            navigate('/note')
+          }
+        }
+        );
+    }
+    }
+
     return(
         <>
         <Top_bar/>
-        <form action='/route' method='post'>
           <div id="Login_Text_Inputs" className='container_text'>
           <input required type="text" className='form_text' name='username' placeholder='username' id='username'></input>
           <input required type="password" className='form_text' name='password' placeholder='password' id='password'></input>
           </div>
           <div className='Login_Buttons'>
-          <input required type="submit" className='login_button'></input>
+          <input required type="submit" className='login_button' onClick={login}></input>
           <Link to="/note"><button className="sign_up_button">Sign Up</button></Link> 
           </div>    
-        </form>
         </>
     )
 }
