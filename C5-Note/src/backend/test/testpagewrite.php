d53802c3e7cf86571d671187dbc397433662f460
fostlia <fostliacat@gmail.com>
2024-10-01 16:28:05 -0400

    Created testpagewrite.php to start working on the backend to save a page's text.

---
+++ C5-Note/src/backend/test/testpagewrite.php
@@ -0,0 +1,24 @@
+<?php
+
+$config = file_get_contents("../config.json");
+$data = json_decode($config);
+
+$username = $data->username;
+$password = $data->password;
+$db_name = $data->db_name;
+
+// Server name is hardcoded
+$server_name = "localhost:3306";
+
+// Start a new mysqli 
+$connection = new mysqli($server_name, $username, $password, $db_name);
+
+if($connection->connect_error) {
+    die("Could not connect to the database");
+}
+
+
+echo "Testing";
+
+
+?>
