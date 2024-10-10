29da4efd03d7fcd106814de1c408381de3b7d4e9
Collin <99486132+CollinOlkows@users.noreply.github.com>
2024-10-10 01:17:39 -0400

    Fixed some issues with font color, attempting to fix font style labels

--- C5-Note/src/Pages/toolbar_testing.jsx
+++ C5-Note/src/Pages/toolbar_testing.jsx
@@ -5,6 +5,8 @@ import './notebooks.css';
 import logo from '../C5.png';
 import { Toolbar } from './toolbar'
 import './toolbar.css';
+import Quill from 'quill';
+
 
 import { useQuill } from "react-quilljs";
 // or const { useQuill } = require('react-quilljs');
@@ -12,12 +14,67 @@ import { useQuill } from "react-quilljs";
 import "quill/dist/quill.snow.css"; // Add css for snow theme
 // or import 'quill/dist/quill.bubble.css'; // Add css for bubble theme
 
+
 export function ToolTest(){
+    const Font = Quill.import("formats/font");
+Font.whitelist = [
+  "arial",
+  "comic-sans",
+  "courier-new",
+  "georgia",
+  "helvetica",
+  "lucida",
+  "Times New Roman"
+];
+Quill.register(Font, true);
 
-    const { quill, quillRef } = useQuill({ placeholder: "Typing...." });
 
-    console.log(quill); // undefined > Quill Object
-    console.log(quillRef); // { current: undefined } > { current: Quill Editor Reference }
+    const theme = 'snow';
+  // const theme = 'bubble';
+
+  const fonts = [
+    { value: 'sans-serif', label: 'Sans Serif' },
+    { value: 'serif', label: 'Serif' },
+    { value: 'monospace', label: 'Monospace' },
+    { value: 'arial', label: 'arial' },
+    { value: 'comic-sans', label: 'comic-sans' },
+    { value: 'Times New Roman', label: 'times-new-roman' }
+];
+
+
+const fontOptions = fonts.map(font => font.value);
+
+
+const modules = {
+    toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ 'font': fontOptions }], // Add the font options here
+        [{ align: [] }],
+        [{ list: 'ordered'}, { list: 'bullet' }, { 'list': 'check' }],
+        [{ indent: '-1'}, { indent: '+1' }],
+        [{ size: ['small', false, 'large', 'huge'] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        ['link', 'image', 'video'],
+        [{ color: [] }, { background: [] }],
+        ['clean'],
+    ],
+    clipboard: {
+        matchVisual: false,
+    },
+};
+
+  const placeholder = 'type here....';
+
+  const formats = [
+    'bold', 'italic', 'underline', 'strike','font',
+    'align', 'list', 'indent',
+    'size', 'header',
+    'link', 'image', 'video',
+    'color', 'background',
+    'clean'
+  ];
+
+  const { quill, quillRef } = useQuill({ theme, modules, formats, placeholder });
 
 
     return(
@@ -47,9 +104,11 @@ export function ToolTest(){
                     <Link to="/"><button className="nbpButtonHome">Copy URL</button></Link>
                 </div>
                 <article className="nbpMain">
+                    <div className="Editor_Area">
                     {/* Lorem Ipsum for filler until note pages implemented */}
-                    <div style={{ width: 500, height: 300 }}>
+                    <div style={{ width: 1200, height: 1000 }}>
                     <div ref={quillRef} />
+                    </div>
                     </div>
                 </article>
                 <aside className="aside nbpSidebarNotebooks">
