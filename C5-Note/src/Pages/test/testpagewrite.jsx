ed6a191ca8325bc3551444580817141995b4058c
fostlia <fostliacat@gmail.com>
2024-10-10 19:33:52 -0400

    Generic "are you sure" prompt when trying to leave.

--- C5-Note/src/Pages/test/testpagewrite.jsx
+++ C5-Note/src/Pages/test/testpagewrite.jsx
@@ -71,38 +71,20 @@ export function TestPageWrite(){
 
     );
 
+    // Generic "are you sure" dialog prompt
+    useEffect(() => {
+        const handleBeforeUnload = (event) => {
+            // Perform actions before the component unloads
+            event.preventDefault();
+            event.returnValue = '';
+        };
+        window.addEventListener('beforeunload', handleBeforeUnload);
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, []);
 
-    // const hasUnsavedChanges = Boolean(contents);
 
-    // useEffect(
-    //     () =>
-    //         navigation.addListener('beforeRemove', (e) => {
-    //             if (!hasUnsavedChanges) {
-    //                 // If we don't have unsaved changes, then we don't need to do anything
-    //                 return;
-    //             }
-    
-    //             // Prevent default behavior of leaving the screen
-    //             e.preventDefault();
-    
-    //             // Prompt the user before leaving the screen
-    //             Alert.alert(
-    //                 'Discard changes?',
-    //                 'You have unsaved changes. Are you sure to discard them and leave the screen?',
-    //                 [
-    //                     { text: "Don't leave", style: 'cancel', onPress: () => {} },
-    //                     {
-    //                     text: 'Discard',
-    //                     style: 'destructive',
-    //                     // If the user confirmed, then we dispatch the action we blocked earlier
-    //                     // This will continue the action that had triggered the removal of the screen
-    //                     onPress: () => navigation.dispatch(e.data.action),
-    //                     },
-    //                 ]
-    //                 );
-    //       }),
-    //     [navigation, hasUnsavedChanges]
-    // );
 
     return(
         <>
