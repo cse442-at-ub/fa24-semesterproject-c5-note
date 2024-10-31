3549d25ce3345e3ac00b5eebbbf22a22feb21840
mdrum617 <mdrum617@gmail.com>
2024-10-31 15:11:35 -0400

    Added http response codes to all outcomes.

--- C5-Note/src/backend/image-upload.php
+++ C5-Note/src/backend/image-upload.php
@@ -18,6 +18,7 @@ else {
 $connection = new mysqli("localhost:3306", $username, $password, $db_name);
 
 if($connection->connect_error) {
+    http_response_code(500);
     die("Could not connect to the database");
 }
 
@@ -32,7 +33,7 @@ if($result->num_rows == 1) {
   $id = $result->fetch_assoc()["user_id"];
 }
 else {
-  http_response_code(400);
+  http_response_code(401);
   die("Invalid token.");
 }
 
@@ -45,29 +46,35 @@ $target_file = $target_dir . $id . "." . $imageFileType;
 // Check if image file is a actual image or fake image
 $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check == false) {
+    http_response_code(400);
     die("File is not an image.");
   }
 
 
 // Check file size less than 2MB
 if ($_FILES["fileToUpload"]["size"] > 2000000) {
+  http_response_code(413);
   die("Sorry, your file is too large.");
 }
 
 // Check file size less than 2MB
 if ($_FILES["fileToUpload"]["size"] == 0) {
+  http_response_code(400);
   die("Sorry, your file is too small.");
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
+  http_response_code(400);
   die("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
 }
 
 
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    http_response_code(200);
     die("File successfully uploaded.");
   } else {
+    http_response_code(500);
     die("Sorry, there was an error uploading your file.");
   }
