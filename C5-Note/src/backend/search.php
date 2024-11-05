9263fa726eb22b87db3bd78d94ccc9b4cc6c9f0e
mdrum617 <mdrum617@gmail.com>
2024-11-05 10:44:27 -0500

    Created search.php

---
+++ C5-Note/src/backend/search.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = file_get_contents("../config.json");
+$data = json_decode($config);
+
+$db_username = $data->username;
+$password = $data->password;
+$db_name = $data->db_name;
+
+$input = json_decode(file_get_contents("php://input"), true);
+$search = $input['search'];
+
+$search = "%" . $search . "%";
+
+$connection = new mysqli("localhost:3306", $db_username, $password, $db_name);
+
+$stmt = $connection->prepare("SELECT username FROM users WHERE UPPER(username) LIKE UPPER(?) ");
+$stmt->bind_param("s", $search);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$rows = array();
+while($row = $result->fetch_assoc()) {
+    array_push($rows, $row["username"]);
+}
+
+http_response_code(200);
+die(json_encode($rows));
