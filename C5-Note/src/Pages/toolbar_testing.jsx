07b7d6eac57e8f49c18733c60668c89d36e9773e
Collin <99486132+CollinOlkows@users.noreply.github.com>
2024-11-26 18:27:52 -0500

    Moved the a element styling to only the notebook page
    Removed buttons from editor that are not usable like AI commands

--- C5-Note/src/Pages/toolbar_testing.jsx
+++ C5-Note/src/Pages/toolbar_testing.jsx
@@ -619,6 +619,8 @@ export function ToolTest(){
 
 
     const config = useMemo(() => ({
+        "buttons": "bold,italic,underline,strikethrough,eraser,ul,ol,font,fontsize,paragraph,lineHeight,superscript,subscript,image,video,spellcheck,speechRecognize,cut,copy",
+        
         cleanHTML: {
             denyTags: {
               script: true,
