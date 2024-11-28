188180c449090d82388974d04075f4915fbe4cf7
fostlia <fostliacat@gmail.com>
2024-11-28 18:10:15 -0500

    Notebook details page's title redirects to /note

--- C5-Note/src/Pages/notebookDetails.jsx
+++ C5-Note/src/Pages/notebookDetails.jsx
@@ -24,7 +24,7 @@ function Top_bar_simple_notes(){
       return(
         <div className='Top_bar'>
           <div className='Top_bar_elms'>
-            <h1 className='Top_bar_text'>C5-Note</h1>
+            <Link to="/note"><h1 className='Top_bar_text'>C5-Note</h1></Link>
             {/*switch the image to be agnostic to database images*/}
             <div className="profile_div">
               <div className="profile_div_color">
