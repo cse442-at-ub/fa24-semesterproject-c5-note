1e5bd9c1b1a36d44c56e8e2023ec09f9c8403f30
Liandra <fostliacat@gmail.com>
2024-12-03 13:24:19 -0500

    Update vite.config.js

--- C5-Note/vite.config.js
+++ C5-Note/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/CSE442/2024-Fall/cse442al/", // /CSE442/2024-Fall/amarteag/ || /CSE442/2024-Fall/cse-442al/
+  base:"/CSE442/2024-Fall/cse-442al/", // /CSE442/2024-Fall/amarteag/ || /CSE442/2024-Fall/cse-442al/
   plugins: [react()],
 })
